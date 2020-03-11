79ff7bd1b61e2bc710c8f0e0830997dd8ecbe0d7
uvsq21918311 <elkadi96hotmail.com>
2020-03-11 10:23:10 +0100

    add checkstyle & javadoc to pom

--- src/main/java/fr/uvsq/pglp_4_1/App.java
+++ src/main/java/fr/uvsq/pglp_4_1/App.java
@@ -6,8 +6,9 @@ package fr.uvsq.pglp_4_1;
  */
 public class App 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        AnnuairePersonnels.main(args);
     }
 }
---
+++ src/test/java/fr/uvsq/pglp_4_1/Personnel.java
@@ -0,0 +1,14 @@
+package fr.uvsq.pglp_4_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Personnel {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
